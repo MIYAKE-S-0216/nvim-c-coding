220c5067e1278ef2e21be0477cba78d28f2d0648
M <M@email.com>
2024-08-31 21:09:08 +0900

    setup.bat added.

---
+++ setup/startup.bat
@@ -0,0 +1,7 @@
+winget install Git.Git
+winget install OpenJS.NodeJS
+winget install equalsraf.neovim-qt
+winget install Neovim.Neovim
+winget install BurntSushi.ripgrep.GNU
+winget install junegunn.fzf
+winget install JFLarvoire.Ag
